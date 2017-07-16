668b1d01e73a1e7094b5404f29020e29acbeffbb
king7854 <king7854@naver.com>
2017-07-16 05:00:57 -0800

    Reset main..

--- pcap_test/main.c
+++ pcap_test/main.c
@@ -1,111 +1,79 @@
 #include <pcap.h>
 #include <stdio.h>
-#include <net/ethernet.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-
-struct ip *iph;
-struct tcphdr *tcph;
-struct ether_header *eph;
-
-
-
-void capturePcap(u_char *useless, const struct pcap_pkthdr *pkthdr, const u_char *packet){
-    unsigned short ether_type;
-    int lengthtmp =0;
-    int length=pkthdr->len;
-
-    eph = (struct ether_header*)packet;
-
-    packet += sizeof(struct ether_header);
-    ether_type = ntohs(eph->ether_type);
-
-    if(ether_type == ETHERTYPE_IP){
-        iph = (struct ip*)packet;
-        printf("Src IP : %s\n", inet_ntoa(iph->ip_src));
-        printf("Dst IP : %s\n", inet_ntoa(iph->ip_dst));
-    }
-}
-
-int main(int argc, char *argv[]){
-        pcap_t *handle;			/* Session handle */
-        char *dev;			/* The device to sniff on */
-        char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-        struct bpf_program fp;		/* The compiled filter */
-        char filter_exp[] = "port 80";	/* The filter expression */
-        bpf_u_int32 mask;		/* Our netmask */
-        bpf_u_int32 net;		/* Our IP */
-        struct pcap_pkthdr header;	/* The header that pcap gives us */
-        const u_char *packet;		/* The actual packet */
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 
+#define SNAP_LEN 1518
+#define SIZE_ETHERNET 14
+#define ETHER_ADDR_LEN 6
 
-        while(1){
-            /* Define the device */
-            dev = pcap_lookupdev(errbuf);
-            if (dev == NULL) {
-                fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-                return(2);
-            }
-            /* Find the properties for the device */
-            if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-                fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
-                net = 0;
-                mask = 0;
-            }/* Define the device */
-            dev = pcap_lookupdev(errbuf);
-            if (dev == NULL) {
-                fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-                return(2);
-            }
-            /* Find the properties for the device */
-            if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
-                fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
-                net = 0;
-                mask = 0;
-            }
-            /* Open the session in promiscuous mode */
-            handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-            if (handle == NULL) {
-                fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-                return(2);
-            }
-            /* Compile and apply the filter */
-            if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-                fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-                return(2);
-            }
-            if (pcap_setfilter(handle, &fp) == -1) {
-                fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-                return(2);
-            }
-            /* Grab a packet */
-            packet = pcap_next(handle, &header);
-            /* Print its length */
-            printf("Jacked a packet with length of [%d]\n", header.len);
-            /* And close the session */
-            pcap_close(handle);
-            /* Open the session in promiscuous mode */
-            handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-            if (handle == NULL) {
-                fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-                return(2);
-            }
-            /* Compile and apply the filter */
-            if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-                fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-                return(2);
-            }
-            if (pcap_setfilter(handle, &fp) == -1) {
-                fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-                return(2);
-            }
-            /* Grab a packet */
-            packet = pcap_next(handle, &header);
-            /* Print its length */
-            printf("Jacked a packet with length of [%d]\n", header.len);
-            /* And close the session */
-            pcap_close(handle);
-        }
-        return(0);
+struct sniff_ethernet {
+        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
+        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
+        u_short ether_type;                     /* IP? ARP? RARP? etc */
+};
+
+struct sniff_ip {
+        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_char  ip_tos;                 /* type of service */
+        u_short ip_len;                 /* total length */
+        u_short ip_id;                  /* identification */
+        u_short ip_off;                 /* fragment offset field */
+        #define IP_RF 0x8000            /* reserved fragment flag */
+        #define IP_DF 0x4000            /* dont fragment flag */
+        #define IP_MF 0x2000            /* more fragments flag */
+        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+        u_char  ip_ttl;                 /* time to live */
+        u_char  ip_p;                   /* protocol */
+        u_short ip_sum;                 /* checksum */
+        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+
+
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+        u_short th_sport;
+        u_short th_dport;
+        tcp_seq th_seq;
+        tcp_seq th_ack;
+        u_char  th_offx2;
+        #define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+        u_char  th_flags;
+        #define TH_FIN  0x01
+        #define TH_SYN  0x02
+        #define TH_RST  0x04
+        #define TH_PUSH 0x08
+        #define TH_ACK  0x10
+        #define TH_URG  0x20
+        #define TH_ECE  0x40
+        #define TH_CWR  0x80
+        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;
+        u_short th_sum;
+        u_short th_urp;
+};
+
+int main(int argc, char **argv){
+
+ char *dev = NULL;
+ char errbuf[PCAP_ERRBUF_SIZE];
+ pcap_t *handle;
+
+ char filter_exp[] = "ip";
+ struct bpf_program fp;
+ bpf_u_int32 mask;
+ bpf_u_int32 net;
+ int num_packets;
+
+return 0;
 }
