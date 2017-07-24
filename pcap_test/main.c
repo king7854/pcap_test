b481278dd0afcc0eb488e27d362399e0d2716772
king7854 <king7854@naver.com>
2017-07-24 04:26:56 -0800

    pcap_loop->pcap_next_ex

--- pcap_test/main.c
+++ pcap_test/main.c
@@ -1,4 +1,3 @@
-
 #include <pcap.h>
 #include <stdio.h>
 #include <string.h>
@@ -26,17 +25,17 @@ struct sniff_ip {
     u_short ip_len;
     u_short ip_id;
     u_short ip_off;
-    #define IP_RF 0x8000
-    #define IP_DF 0x4000
-    #define IP_MF 0x2000
-    #define IP_OFFMASK 0x1fff
+#define IP_RF 0x8000
+#define IP_DF 0x4000
+#define IP_MF 0x2000
+#define IP_OFFMASK 0x1fff
     u_char  ip_ttl;
     u_char  ip_p;
     u_short ip_sum;
     struct  in_addr ip_src, ip_dst;
 };
-    #define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
-    #define IP_V(ip)                (((ip)->ip_vhl) >> 4)
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
 
 
 typedef u_int tcp_seq;
@@ -47,17 +46,17 @@ struct sniff_tcp {
     tcp_seq th_seq;
     tcp_seq th_ack;
     u_char  th_offx2;
-    #define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
     u_char  th_flags;
-    #define TH_FIN  0x01
-    #define TH_SYN  0x02
-    #define TH_RST  0x04
-    #define TH_PUSH 0x08
-    #define TH_ACK  0x10
-    #define TH_URG  0x20
-    #define TH_ECE  0x40
-    #define TH_CWR  0x80
-    #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
+#define TH_ACK  0x10
+#define TH_URG  0x20
+#define TH_ECE  0x40
+#define TH_CWR  0x80
+#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
     u_short th_win;
     u_short th_sum;
     u_short th_urp;
@@ -103,12 +102,9 @@ void hextoascii(const u_char *payload, int len, int offset) {
     printf("\n");
 }
 
-void capturePacket(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
+void packetCapture() {
 
-    static int count = 1;
-    const struct sniff_ethernet *ethernet;
-    const struct sniff_ip *ip;
-    const struct sniff_tcp *tcp;
+    const u_char *packet;
     const char *payload;
     char srcbuf[20];
     char dstbuf[20];
@@ -119,68 +115,104 @@ void capturePacket(u_char *args, const struct pcap_pkthdr *header, const u_char 
 
     int line_len;
     int offset = 0;
+    int num=0;
 
-    /* inet_ntoa -> inet_ntop */
-    //printf("Src IP: %s\n", inet_ntoa(ip->ip_src));
-    //printf("Dst IP: %s\n", inet_ntoa(ip->ip_dst));
-    printf("Src IP: %s\n", inet_ntop(AF_INET, &(ip->ip_src), srcbuf, sizeof(srcbuf)));
-    printf("Dst IP: %s\n", inet_ntop(AF_INET, &(ip->ip_dst), dstbuf, sizeof(dstbuf)));
+     struct sniff_ethernet *ethernet;
+     struct sniff_ip *ip;
+     struct sniff_tcp *tcp;
+    struct pcap_pkthdr *header;
+
+    pcap_t *handle;
+    //char *dev = "eth0";
+    char errbuf[PCAP_ERRBUF_SIZE];
+    ethernet = (struct sniff_ethernet *)(packet);
 
 
-    ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
-    size_ip = IP_HL(ip) * 4;
+    handle = pcap_open_live("eth0", SNAP_LEN, 1, 1000, errbuf);
 
-    printf("Src IP: %s\n", inet_ntoa(ip->ip_src));
-    printf("Dst IP: %s\n", inet_ntoa(ip->ip_dst));
+    while(pcap_next_ex(handle, &header, &packet)){
+        if(num>5)
+            break;
+        num++;
+        printf("%d\n", num);
+        /* inet_ntoa -> inet_ntop */
+        //printf("Src IP: %s\n", inet_ntoa(ip->ip_src));
+        //printf("Dst IP: %s\n", inet_ntoa(ip->ip_dst));
+        //printf("Src IP: %s\n", inet_ntop(AF_INET, &(ip->ip_src), srcbuf, sizeof(srcbuf)));
+        //printf("Dst IP: %s\n", inet_ntop(AF_INET, &(ip->ip_dst), dstbuf, sizeof(dstbuf)));
 
-    tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
-    size_tcp = TH_OFF(tcp) * 4;
-
-    printf("Src port: %d\n", ntohs(tcp->th_sport));
-    printf("Dst port: %d\n", ntohs(tcp->th_dport));
-
-    payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
-
-    size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
-
-    if (size_payload > 0) {
-        printf("Data :\n");
-
-        if (size_payload <= 16) {
-            hextoascii(payload, size_payload, offset);
-            return;
+        for(int i=0; i<6; i++){
+              printf("%02x", ethernet->ether_shost[i]);
+              if(i!=5)
+                  printf(":");
         }
+        printf("\n");
 
-        for (;; ) {
-            line_len = 16 % size_payload;
-            hextoascii(payload, line_len, offset);
-            size_payload = size_payload - line_len;
-            payload = payload + line_len;
-            offset = offset + 16;
+        for(int i=0; i<6; i++){
+              printf("%02x", ethernet->ether_dhost[i]);
+              if(i!=5)
+                  printf(":");
+        }
+        printf("\n");
+
+        ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);
+        size_ip = IP_HL(ip) * 4;
+
+        printf("Src IP: %s\n", inet_ntoa(ip->ip_src));
+        printf("Dst IP: %s\n", inet_ntoa(ip->ip_dst));
+
+        tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);
+        size_tcp = TH_OFF(tcp) * 4;
+
+        printf("Src port: %d\n", ntohs(tcp->th_sport));
+        printf("Dst port: %d\n", ntohs(tcp->th_dport));
+
+        payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);
+
+        size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+
+        if (size_payload > 0) {
+            printf("Data :\n");
+
             if (size_payload <= 16) {
                 hextoascii(payload, size_payload, offset);
-                break;
+                return;
+            }
+
+            for (;; ) {
+                line_len = 16 % size_payload;
+                hextoascii(payload, line_len, offset);
+                size_payload = size_payload - line_len;
+                payload = payload + line_len;
+                offset = offset + 16;
+                if (size_payload <= 16) {
+                    hextoascii(payload, size_payload, offset);
+                    break;
+                }
             }
         }
     }
+    pcap_close(handle);
 }
 
 int main(int argc, char **argv)
 {
-    char *dev;
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *handle;
 
-    char filter_exp[] = "tcp";
-    struct bpf_program fp;
-    bpf_u_int32 mask;
-    bpf_u_int32 net;
 
-    handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
+    //handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
 
-    pcap_loop(handle, 5, capturePacket, NULL);
+    //capturePacket;
+    //pcap_loop(handle, 5, capturePacket, NULL);
 
-    pcap_close(handle);
+    /*while(pcap_next_ex(handle, &handle, &pkt_data)){
+        if(i>5)
+            return;
+        capturePacket;
+        i++;
+    }*/
+
+    packetCapture();
+
 
     return 0;
 }
